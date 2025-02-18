ff5254bd73f31c7e90277a131267649c4271433d
user7210unix <user7210@proton.me>
2025-02-18 12:02:23 +0100

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -2,8 +2,7 @@
 
 # simple screen information script
 
-wms=(i3 berry dwm openbox twobwm pekwm simplewm labwc simplewc)
-bars=(waybar polybar)
+wms=(berry awesome hyprland fusionwm dwm openbox twobwm pekwm)
 
 # define colors for color-echo
 red="\e[31m"
@@ -15,11 +14,12 @@ prp="\e[35m"
 rst="\e[0m"
 
 color-echo() {  # print with colors
-      printf "\e[22C%s$cyn%-12s  $rst%s\n" "$1" "$2" "$3"
+   printf "$red$1$cyn%10s : $rst$3\n" "$2"
 }
 
+
 print-kernel() {
-   color-echo "├ " "Kernel" "$(uname -smr)"
+   color-echo "$1" "Kernel" "$(uname -smr)"
 }
 
 print-uptime() {
@@ -29,30 +29,30 @@ print-uptime() {
    hours=$((${up}/3600%24))    # seconds divided by 3600 mod 24 is hours
    mins=$((${up}/60%60))       # seconds divided by 60 mod 60 is mins
    
-   color-echo "Uptime" "$(echo $days'd '$hours'h '$mins'm')"
+   color-echo "$1" "Uptime" "$(echo $days'd '$hours'h '$mins'm')"
 }
 
 print-shell() {
-   color-echo "└ "  "Shell" $SHELL
-}
-
-print-term() {
-   color-echo "├ " "Terminal" $TERM
+   color-echo "$1" "Shell" $SHELL
 }
 
 print-cpu() {
    cpu=$(grep -m1 -i 'model name' /proc/cpuinfo)
 
-   color-echo "├ " "CPU" "${cpu#*: }" # everything after colon is processor name
+   color-echo "$1" "CPU" "${cpu#*: }" # everything after colon is processor name
 }
 
 print-disk() {
+   # field 2 on line 2 is total, field 3 on line 2 is used
    disk=$(df -h / | awk 'NR==2 {total=$2; used=$3; print used" / "total}')
    
-   color-echo "Disk" "$disk"
+   color-echo "$1" "Disk" "$disk"
 }
 
 print-mem() {
+   # field 2 on line 2 is total, field 3 on line 2 is used (in new format)
+   # field 2 on line 2 is total, field 3 on line 3 is used (in old format)
+   # use -m because slackware does not have -h
 
    if [[ $(free -m) =~ "buffers" ]]; then # using old format
       mem=$(free -m | awk 'NR==2 {total=$2} NR==3 {used=$3; print used"M / "total"M"}')
@@ -60,91 +60,70 @@ print-mem() {
       mem=$(free -m | awk 'NR==2 {total=$2} NR==2 {used=$3; print used"M / "total"M"}')
    fi
 
-   color-echo "├ " "Mem" "$mem"
+   color-echo "$1" "Mem" "$mem"
 }
 
 print-wm() {
    for wm in ${wms[@]}; do          # pgrep through wmname array
       pid=$(pgrep -x -u $USER $wm) # if found, this wmname has running process
    if [[ "$pid" ]]; then
-      color-echo "├ " "WM" $wm
+      color-echo "$1" "WM" $wm
       break
    fi
 done
 }
 
-print-bar() {
-   for bar in ${bars[@]}; do
-      pid=$(pgrep -x -u $USER $bar)
-   if [[ "$pid" ]]; then
-      color-echo "├ " "Bar" $bar
-      break
-   fi
-   done
-}
-
 print-distro() {
    [[ -e /etc/os-release ]] && source /etc/os-release
    if [[ -n "$PRETTY_NAME" ]]; then
-      color-echo "├ " "OS" "$PRETTY_NAME" 
+      color-echo "$1" "OS" "$PRETTY_NAME"
    else
-      color-echo "├ " "OS" "not found"
+      color-echo "$1" "OS" "not found"
    fi
+}
+
+print-packages() {
+   color-echo "$1"  "Packages" "$(ls -1 /home/user/sources/ | wc -l) (pfstool)"
 }
 
 print-resolution() {
-   if [[ $DISPLAY ]]; then
-      res=$(xwininfo -root | grep 'geometry' | awk '{print $2}' | cut -d+ -f1)
-   else
-      for dev in /sys/class/drm/*/modes; do
-         read -r single_res _ < "$dev"
-
-         [[ $single_res ]] && res="${single_res}"
-      done
-   fi
-
-   color-echo "├ " "Resolution" $res
+   res=$(xwininfo -root | grep 'geometry' | awk '{print $2}' | cut -d+ -f1)
+   
+   color-echo "$1" "Resolution" $res
 }
 
 print-colors() {
+   xrdb -load $HOME/.Xdefaults
 
-   printf "\e[%bC" "31" 
-   for i in {0..7}; do echo -en "\e[$((30+$i))m\uf111 ${colors[i]} \e[0m"; done
+   colors=($(xrdb -query | sed -n 's/.*color\([0-9]\)/\1/p' | sort -nu | cut -f2))
+
+   echo -e "\e[1;37m
+ Black      Red        Green      Yellow     Blue       Magenta    Cyan       White
+ ───────────────────────────────────────────────────────────────────────────────────────\e[0m"
+   for i in {0..7}; do echo -en "\e[$((30+$i))m █ ${colors[i]} \e[0m"; done
    echo
+   for i in {8..15}; do echo -en "\e[1;$((22+$i))m █ ${colors[i]} \e[0m"; done
+   echo -e "\n"
 
+#   NAMES=('black' 'red' 'green' 'yellow' 'blue' 'magenta' 'cyan' 'white')
+#   for f in {0..7}; do
+#      echo -en "\033[m\033[$(($f+30))m ${NAMES[$f]} " # normal colors
+#   done
+#   echo
+#   for f in {0..7}; do
+#      echo -en "\033[m\033[1;$(($f+30))m ${NAMES[$f]} " # bold colors
+#   done
+#   echo -e "$rst\n"
 }
 
-print-image() {
-   if [ -z $1 ]; then
-      printf "$red%10s$rst\n" '                 	'
-      printf "$red%10s$rst\n" '   █     █▀▀▀  █▀▀▀█	'
-      printf "$red%10s$rst\n" '   █     █▀▀▀  ▀▀▀▄▄	'
-      printf "$red%10s$rst\n" '   █▄▄█  █     █▄▄▄█	'
-      printf "$red%10s$rst\n" '    			'
-      printf "$red%10s$rst\n" '    			'
-      printf "$red%10s$rst\n" '   			'
-      printf '\e[%sA\e[9999999D' "${lines:-9}"
-   elif [ $TERM = "xterm-kitty" ]; then
-      kitty +kitten icat --align left --place 40x40@0x5 $1
-      printf '\e[%sA\e[9999999D' "-11"
-   elif [ $TERM = "foot" ]; then
-      echo -n "  " && img2sixel -w 140 $1
-      printf '\e[%sA\e[9999999D' "${lines:-11}"
-   fi
-}
-
-print-image $1
-
-printf "\e[21C%s\n" "System:"
-print-distro
-print-kernel
-print-cpu
-print-mem
-print-wm
-print-bar
-print-resolution
-print-term
-print-shell
 echo
-print-colors
+print-distro      ' 	           '
+print-kernel      '  █     █▀▀▀  █▀▀▀█ '
+print-cpu         '  █     █▀▀▀  ▀▀▀▄▄ '
+print-mem         '  █▄▄█  █     █▄▄▄█ '
+print-shell       '                    '
+print-wm          '      	       '
+
 echo
+#print-colors
+
